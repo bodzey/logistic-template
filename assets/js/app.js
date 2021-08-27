46003e0b4909c7960d754bb70e12f3bbabb93166
Bodzey <djsteward.rv@gmail.com>
2021-08-27 19:48:00 +0300

    add slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -138,6 +138,41 @@ $(function () {
         }, 200);
     }
     
+    /* Slick Slider
+    =====================================*/
 
-    
+    let introSlider = $('#introSlider')
+
+    introSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1000
+    });
+			
+    $('#introSliderPrev').on('click', function () {
+        introSlider.slick('slickPrev')
+    })
+
+    $('#introSliderNext').on('click', function () {
+        introSlider.slick('slickNext')
+    })
+
+
+    let reviewsSlider = $('#reviewsSlider')
+
+    reviewsSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1000
+    });
 })

73d622ef8d5ccb9c2f4b385ec17367eb819517c6
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2021-08-27 19:56:07 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,4 @@
 /* jQuery */
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-    let viewportmeta = document.querySelector('meta[name="viewport"]')
-    if (viewportmeta) {
-        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0'
-        document.body.addEventListener('gesturestart', function () {
-            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6'
-        }, false)
-    }
-}
-
 
 
 $(function () {

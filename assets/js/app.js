e2d393f95bb17c535400f02e13b88e4d8f521324
Bodzey <djsteward.rv@gmail.com>
2021-08-30 02:35:04 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,27 @@
 
 $(function () {
 
+
+
+    /* Nav Toogle
+    ==========================*/
+    let navToggle = $('#navToggle')
+    let nav = $('#nav')
+    
+    navToggle.on('click', function (event) {
+        event.preventDefault()
+
+        $('body').toggleClass('show-nav')
+        $(this).toggleClass('active')
+        nav.toggleClass('show')
+    })
+
+    $(window).on("resize", function () {
+        $('body').removeClass('show-nav')
+        navToggle.removeClass('active')
+        nav.removeClass('show')
+    })
+
     let intro = $("#intro")
     let header = $("#header")
     let introH = intro.innerHeight()
@@ -39,6 +60,10 @@ $(function () {
 
         let scrollEl = $(this).data("scroll")
         let scrollElPos = $(scrollEl).offset().top
+
+        $('body').removeClass('show-nav')
+        navToggle.removeClass('active')
+        nav.removeClass('show')
 
         console.log(scrollElPos)
         $("html, body").animate({
@@ -165,4 +190,36 @@ $(function () {
         autoplaySpeed: 4000,
         speed: 1000
     });
+
+    /* AOS Animation
+    =========================*/
+
+    AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+
+
+
+
 })
